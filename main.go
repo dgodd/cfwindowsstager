ae5ca28fd35c50e503d55f145fa516687ed78653
Dave Goddard <dave@goddard.id.au>
2019-01-10 14:04:42 +1100

    determine start command

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"archive/tar"
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -58,30 +60,24 @@ func stage() error {
 	client, err := NewClient()
 
 	ctx := context.Background()
-	ctr, err := client.ContainerCreate(
-		ctx,
-		&container.Config{
-			Image: "cflinuxfs3wbal",
-			Cmd: []string{
-				"/tmp/lifecycle/builder",
-				"-buildDir=/app",
-				"-buildpacksDir=/tmp/buildpacks",
-				"-outputDroplet=/tmp/droplet",
-				"-outputMetadata=/tmp/result.json",
-				// "-skipDetect",
-				"-buildpackOrder=https://github.com/cloudfoundry/ruby-buildpack/releases/download/v1.7.29/ruby-buildpack-cflinuxfs3-v1.7.29.zip", // comma-separated list of buildpacks, to be tried in order
-
-				// -buildArtifactsCacheDir string
-				// directory where previous cached build artifacts should be extracted (default "/tmp/cache")
-				// -skipCertVerify
-				// skip SSL certificate verification
-			},
+	ctr, err := client.ContainerCreate(ctx, &container.Config{
+		Image: "cflinuxfs3wbal",
+		Cmd: []string{
+			"/tmp/lifecycle/builder",
+			"-buildDir=/app",
+			"-buildpacksDir=/tmp/buildpacks",
+			// "-buildArtifactsCacheDir=/tmp/cache",
+			"-outputDroplet=/tmp/droplet",
+			"-outputMetadata=/tmp/result.json",
+			// "-skipDetect",
+			"-buildpackOrder=https://github.com/cloudfoundry/ruby-buildpack/releases/download/v1.7.29/ruby-buildpack-cflinuxfs3-v1.7.29.zip", // comma-separated list of buildpacks, to be tried in order
+			// "-skipCertVerify", // skip SSL certificate verification
 		},
-		nil, // &container.HostConfig{
-		// 	Binds: []string{
-		// 		fmt.Sprintf("%s:%s:", b.CacheVolume, launchDir),
-		// 	},
-		nil, "")
+	}, &container.HostConfig{
+		// Binds: []string{
+		// 	fmt.Sprintf("%s:%s:", "dgodd-ruby-app-cache-FIXME", "/tmp/cache"),
+		// },
+	}, nil, "")
 	if err != nil {
 		return errors.Wrap(err, "container create")
 	}
@@ -100,7 +96,48 @@ func stage() error {
 		return errors.Wrap(err, "container run")
 	}
 
+	// FIXME need to copy files from this container to the image. Assume better to copy directly
+	// if rc, _, err := client.CopyFromContainer(ctx, ctr.ID, "/tmp/droplet"); err != nil {
+	// 	return errors.Wrap(err, "copy droplet out")
+	// } else {
+	// 	defer rc.Close()
+	// 	f, _ := os.Create("/tmp/droplet.tar")
+	// 	defer f.Close()
+	// 	_, _ = io.Copy(f, rc)
+	// }
+
+	startCommand, err := ResultJSONProcessType(client, ctx, ctr.ID, "/tmp/result.json")
+	if err != nil {
+		return errors.Wrap(err, "find start command")
+	}
+	fmt.Println("START COMMAND:", startCommand)
+
 	return nil
+}
+
+func ResultJSONProcessType(client *dockercli.Client, ctx context.Context, ctrID string, path string) (string, error) {
+	rc, _, err := client.CopyFromContainer(ctx, ctrID, path)
+	if err != nil {
+		return "", errors.Wrap(err, fmt.Sprintf("copy single file (%s) out of container", path))
+	}
+	defer rc.Close()
+	tr := tar.NewReader(rc)
+	_, err = tr.Next()
+	if err == io.EOF {
+		return "", errors.New("result.json not found in container")
+	}
+	if err != nil {
+		return "", errors.Wrap(err, "parsing result.json tar from container")
+	}
+	result := struct {
+		ProcessTypes struct {
+			Web string `json:"web"`
+		} `json:"process_types"`
+	}{}
+	if err := json.NewDecoder(tr).Decode(&result); err != nil {
+		return "", errors.Wrap(err, "parsing result.json from container")
+	}
+	return result.ProcessTypes.Web, nil
 }
 
 func main() {
